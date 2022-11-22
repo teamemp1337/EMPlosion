187d5069ed1aefaf8edfcad396a8a395b08a1e5e
Richard Wahidi <richard.wahidi@nps.edu>
2022-11-22 12:21:19 -0800

    12th commit

--- EMPlosion.py
+++ EMPlosion.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import streamlit as st
+from ipyvizzu import Chart, Data, Config, Style
+from streamlit.components.v1 import html
 from st_aggrid import AgGrid, GridOptionsBuilder
+from st_vizzu import *
 
 # Make the default width setting span the entire screen
 st.set_page_config(layout="wide")
@@ -54,6 +57,70 @@ response = AgGrid(
     allow_unsafe_jscode=True,
     enable_enterprise_modules=True
 )
+
+def create_chart():
+
+    # initialize chart
+    chart = Chart(width="640px", height="360px", display="manual")
+
+    # add data
+    data = Data()
+    data_frame = pd.read_csv("https://github.com/vizzuhq/ipyvizzu/raw/main/docs/examples/stories/titanic/titanic.csv")
+    data.add_data_frame(data_frame)
+
+    chart.animate(data)
+
+    # add config
+    chart.animate(Config({"x": "Count", "y": "Sex", "label": "Count","title":"Passengers of the Titanic"}))
+    chart.animate(Config({"x": ["Count","Survived"], "label": ["Count","Survived"], "color": "Survived"}))
+    chart.animate(Config({"x": "Count", "y": ["Sex","Survived"]}))
+
+    # add style
+    chart.animate(Style({"title": {"fontSize": 35}}))
+
+    return chart._repr_html_()
+
+CHART = create_chart()
+html(CHART, width=650, height=370)
+
+# Load Data
+df9 = pd.read_csv("music_data.csv", index_col=0)
+# Create ipyvizzu Object with the DataFrame
+obj = create_vizzu_obj(df9)
+
+# Preset plot usage. Preset plots works directly with DataFrames.
+bar_obj = bar_chart(df9,
+            x = "Kinds",
+            y = "Popularity",
+            title= "1.Using preset plot function `bar_chart()`"
+            )
+
+# Animate with defined arguments
+anim_obj = beta_vizzu_animate( bar_obj,
+    x = "Genres",
+    y =  ["Popularity", "Kinds"],
+    title = "Animate with beta_vizzu_animate () function",
+    label= "Popularity",
+    color="Genres",
+    legend="color",
+    sort="byValue",
+    reverse=True,
+    align="center",
+    split=False,
+)
+
+# Animate with general dict based arguments
+_dict = {"size": {"set": "Popularity"},
+    "geometry": "circle",
+    "coordSystem": "polar",
+    "title": "Animate with vizzu_animate () function",
+    }
+anim_obj2 = vizzu_animate(anim_obj,_dict)
+
+# Visualize within Streamlit
+with st.container(): # Maintaining the aspect ratio
+    st.button("Animate")
+    vizzu_plot(anim_obj2)
 
 # if "mdf" not in st.session_state:
 #     st.session_state.mdf = pd.DataFrame(
